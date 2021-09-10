ccfee9688aca211a12d6f678f502c877641d1120
s3ng0z <jandres.gomez@edu.uah.es>
2021-09-10 03:11:49 +0200

    commit

--- scripts/executeUpdates.sh
+++ scripts/executeUpdates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git init
 cd ..
-sudo git pull https://s3ng0z-x120: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@github.com/s3ng0z-x120/SecurityPi_RaspPi.git master
+git pull https://s3ng0z-x120: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@github.com/s3ng0z-x120/RaspyServerGithub.git master
 cd src/
