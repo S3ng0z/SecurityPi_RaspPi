2d7f8acc17fcd0a5c1babcf556cfd53d2b3b0bba
s3ng0z <jandres.gomez@edu.uah.es>
2021-12-06 21:41:28 +0100

    commit

--- src/models/HomeModel.py
+++ src/models/HomeModel.py
@@ -19,6 +19,7 @@ import collections
 import imutils
 from imutils.video import VideoStream
 import pickle
+import socket
 #import git
 
 path = ''
@@ -91,158 +92,32 @@ class HomeModel:
         pass
 
     def workerCAM(self, lproxy):
-        if(lproxy.get('killAll') != 0):
-            aux = Connection()
-            socket = aux.connect()
-            conn = socket.makefile('wb')
-            try:
-                temp_name = next(tempfile._get_candidate_names())
-                #camera = picamera.PiCamera()
-                #camera = VideoStream(usePiCamera=1).start()
-                #camera.vflip = True
-                #camera.resolution = (1280, 720)
-                #camera.resolution = (1280, 720)
-                # Start a preview and let the camera warm up for 2 seconds
-                #camera.start_preview()
-                
-                cap = cv2.VideoCapture(0)
-                cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-                cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-                cap.set(cv2.CAP_PROP_FPS, 40)
-                
-                time.sleep(2)
-                '''
-                detection_graph = tf.Graph()
-                contFrames = 0
-                with detection_graph.as_default():
-                    od_graph_def = tf.GraphDef()
-                    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-                        serialized_graph = fid.read()
-                        od_graph_def.ParseFromString(serialized_graph)
-                        tf.import_graph_def(od_graph_def, name='')
-                '''
-                #detection of video
-                '''
-                with detection_graph.as_default():
-                    with tf.Session(graph=detection_graph) as sess'''
-                stream = io.BytesIO()
-                #rawCapture = PiRGBArray(camera, size=(640, 480))
-                md = SingleMotionDetector(accumWeight=0.1)
-                total = 0
-                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-                img_counter = 0
-                #for frame in camera.capture_continuous(stream, 'jpeg'):
-                #for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-                while True:
-                    if(lproxy.get('killAll') == 0):
-                        break
-                    else:
-                        #data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-                        #image = cv2.imdecode(data, 1)
-                        '''
-                        frame = cap.read()
-                        frame = imutils.resize(frame, width=720)
-                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-                        # grab the current timestamp and draw it on the frame
-                        img_str = cv2.imencode('.jpg', frame)[1].tostring()
-                        timestamp = datetime.now()
-                        cv2.putText(frame, timestamp.strftime(
-                            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-                        # update the background model and increment the total number
-                        # of frames read thus far
-                        md.update(gray)
-                        if(total%10 == 0):
-                            # detect motion in the image
-                            motion = md.detect(gray)
 
-                            # cehck to see if motion was found in the frame
-                            if motion is not None:
-                                # unpack the tuple and draw the box surrounding the
-                                # "motion area" on the output frame
-                                (thresh, (minX, minY, maxX, maxY)) = motion
-                                cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-                                                    (0, 0, 255), 2)
-                            
-                        
-                        total += 1
-                        
-                        result, frame = cv2.imencode('.jpg', frame, encode_param)
-                        '''
-                        ret, frame = cap.read()
-                        result, frame = cv2.imencode('.jpg', frame, encode_param)
-                    #    data = zlib.compress(pickle.dumps(frame, 0))
-                        data = pickle.dumps(frame, 0)
-                        size = len(data)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('192.168.1.124', 8485))
+        connection = client_socket.makefile('wb')
 
-                        print("{}: {}".format(img_counter, size))
-                        socket.sendall(struct.pack(">L", size) + data)
-                        img_counter += 1
-                        '''
-                        conn.write(struct.pack('<L', stream.tell()))
-                        conn.flush()
+        cam = cv2.VideoCapture(0)
 
-                        stream.seek(0)
-                        conn.write(stream.read())
+        cam.set(3, 320);
+        cam.set(4, 240);
 
-                        stream.seek(0)
-                        stream.truncate()
-                        if cv2.waitKey(1) == ord('q'):
-                            print('Paso por aquí')
-                            break
-                        '''
-                        # Write a length of zero to the stream to signal we're done
-                        #conn.write(struct.pack('<L', 0))
-                        
-                        
-                        '''
-                        temp_name = next(tempfile._get_candidate_names()) + '.jpg'
-                        # Construct a numpy array from the stream
-                        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-                        # "Decode" the image from the array, preserving colour
-                        image = cv2.imdecode(data, 1)
-                        imS = cv2.resize(image, (1280, 720)) # Resize image
-                        #cv2.imwrite(temp_name, imS)
-                        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                        image_np_expanded = np.expand_dims(imS, axis=0)
-                        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                        # Each box represents a part of the image where a particular object was detected.
-                        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                        # Each score represent how level of confidence for each of the objects.
-                        # Score is shown on the result image, together with the class label.
-                        scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                        classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                        # Actual detection.
-                        (boxes, scores, classes, num_detections) = sess.run(
-                            [boxes, scores, classes, num_detections],
-                            feed_dict={image_tensor: image_np_expanded})
-                        if(num_detections > 0):
-                            print('num_detections ', num_detections)
-                            if not os.path.isdir(APP_PATH+'/store'):
-                                os.mkdir(APP_PATH+'/store')
-                            cv2.imwrite((APP_PATH+'/store/'+temp_name), imS)
-                            
-                        #conn.write(struct.pack('<L', stream.tell()))
-                        #conn.flush()
-                        
-                        stream.seek(0)
-                        #conn.write(stream.read())
-                        
-                        stream.seek(0)
-                        stream.truncate()
-                        if cv2.waitKey(1) == ord('q'):
-                            print('Paso por aquí')
-                            break
-                
-                        # Write a length of zero to the stream to signal we're done
-                        #conn.write(struct.pack('<L', 0))
-                        '''
-            finally:
-                conn.close()
-                aux.closeConn(socket)
-                gc.collect()
+        img_counter = 0
+
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+        while True:
+            ret, frame = cam.read()
+            result, frame = cv2.imencode('.jpg', frame, encode_param)
+        #    data = zlib.compress(pickle.dumps(frame, 0))
+            data = pickle.dumps(frame, 0)
+            size = len(data)
+
+            print("{}: {}".format(img_counter, size))
+            client_socket.sendall(struct.pack(">L", size) + data)
+            img_counter += 1
+
+        cam.release()
         pass
   
     def workerReviewScreenshots(self, lproxy):
