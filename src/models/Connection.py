8fc6b5620e58941e367c8d5e63cfb38805b1a5cc
s3ng0z <jandres.gomez@edu.uah.es>
2022-02-06 17:18:14 +0100

    commit

--- src/models/Connection.py
+++ src/models/Connection.py
@@ -17,7 +17,7 @@ class Connection:
         #clientSocket.connect((self.URL, self.PORT))
         #clientSocket.connect(('192.168.228.31', 8000))
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect(('192.168.1.33', 8000))
+        clientSocket.connect(('192.168.1.33', 8080))
         clientSocket.makefile('wb')
 
         return clientSocket
