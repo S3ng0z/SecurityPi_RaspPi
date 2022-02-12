104061a433a617ea8522b9a2be83ffef02b81545
s3ng0z <jandres.gomez@edu.uah.es>
2022-02-12 17:17:33 +0100

    commit change ip server

--- src/models/Connection.py
+++ src/models/Connection.py
@@ -3,7 +3,7 @@ import socket
 
 class Connection:
 
-    URL = '192.168.1.33'
+    URL = '192.168.0.22'
     PORT = 8000
 
     def __init__(self):
@@ -17,7 +17,7 @@ class Connection:
         #clientSocket.connect((self.URL, self.PORT))
         #clientSocket.connect(('192.168.228.31', 8000))
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect(('192.168.1.33', 8080))
+        clientSocket.connect(('192.168.0.22', 8080))
         clientSocket.makefile('wb')
 
         return clientSocket
