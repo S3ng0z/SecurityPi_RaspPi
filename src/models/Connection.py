1e306fcd4b4de66f648e1488fd8f1e1ece3bece5
s3ng0z <jandres.gomez@edu.uah.es>
2022-02-14 03:06:47 +0100

    commit

--- src/models/Connection.py
+++ src/models/Connection.py
@@ -4,7 +4,7 @@ import socket
 class Connection:
 
     URL = '192.168.0.22'
-    PORT = 8080
+    PORT = 8000
 
     def __init__(self):
         pass
@@ -17,7 +17,7 @@ class Connection:
         #clientSocket.connect((self.URL, self.PORT))
         #clientSocket.connect(('192.168.228.31', 8000))
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect(('192.168.0.22', 8080))
+        clientSocket.connect(('192.168.0.22', 8000))
         clientSocket.makefile('wb')
         print('clientSocket: ' + str(clientSocket))
         return clientSocket
