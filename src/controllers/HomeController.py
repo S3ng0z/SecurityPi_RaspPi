adf5646ef6af05e08114687106bf95e9ea320486
s3ng0z <jandres.gomez@edu.uah.es>
2022-02-24 10:07:19 +0100

    commit

--- src/controllers/HomeController.py
+++ src/controllers/HomeController.py
@@ -255,7 +255,7 @@ class HomeController(Controller):
                         #clientSocket.send(image)
                         image = cv2.imread(APP_PATH+'/frame_container/'+filename, cv2.IMREAD_GRAYSCALE)
 
-                        if not image:      # always check for None
+                        if image is None:      # always check for None
                             break
                         
                         imageToEncode = self.homeModel.encodeImage(image, encode_param)
