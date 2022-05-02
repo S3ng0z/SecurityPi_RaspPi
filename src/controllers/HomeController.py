c7bff9c009f908c47f30ec5a37ad9d4b861a5b6f
s3ng0z <jandres.gomez@edu.uah.es>
2022-05-02 13:43:36 +0200

    commit

--- src/controllers/HomeController.py
+++ src/controllers/HomeController.py
@@ -139,7 +139,7 @@ class HomeController(Controller):
         print('clientSocket: ' + str(clientSocket))
         camera = self.homeModel.connectCamera()
 
-        pathHaarcascade = APP_PATH + '/lib/haarcascade_upperbody.xml'
+        pathHaarcascade = APP_PATH + '/lib/haarcascade_fullbody.xml'
         faceCascade = cv2.CascadeClassifier(pathHaarcascade)
 
         camera.start_preview()
