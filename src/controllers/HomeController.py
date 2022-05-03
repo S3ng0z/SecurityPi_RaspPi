bcec6f2792cfc872ba71720f39eee8a7e2a49cdc
s3ng0z <jandres.gomez@edu.uah.es>
2022-05-03 03:10:00 +0200

    commit

--- src/controllers/HomeController.py
+++ src/controllers/HomeController.py
@@ -193,7 +193,7 @@ class HomeController(Controller):
             y0, dy = 50, 4
             for i, line in enumerate(avg_fps.split('\n')):
                 y = y0 + i*dy
-                cv2.putText(image, line, (50, y ), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+                cv2.putText(image, line, (50, y ), font, 0.7, (255, 255, 255), 3, cv2.LINE_AA)
             
         
             # putting the FPS count on the frame
