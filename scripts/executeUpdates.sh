12b43f1049841406db8661dbf260333f3dc91104
Andres Gomez <jandres.gomez@edu.uah.es>
2022-12-18 17:15:42 +0100

    commit

--- scripts/executeUpdates.sh
+++ scripts/executeUpdates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git init
 cd ..
-git pull https://s3ng0z-x120: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@github.com/s3ng0z-x120/SecurityPi_RaspPi.git master
+#git pull https://s3ng0z-x120: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@github.com/s3ng0z-x120/SecurityPi_RaspPi.git master
 cd src/
